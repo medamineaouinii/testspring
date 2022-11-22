775956a4c0ffac7f9fd744d6ba79ef12fa7cfe3f
unknown <mohamedamine.aouini@esprit.tn>
2022-11-23 09:25:17 +1300

    first commit

---
+++ src/main/java/com/example/aouiniamine/AouiniAmineApplication.java
@@ -0,0 +1,13 @@
+package com.example.aouiniamine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AouiniAmineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AouiniAmineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aouiniamine/AouiniAmineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aouiniamine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AouiniAmineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
